895643435e535fffe453b125f49fc0afb750f3e8
RANDEEP2921 <52604421+RANDEEP2921@users.noreply.github.com>
2021-07-02 13:52:42 +0530

    My project

---
+++ app/src/main/java/com/simraniks/gocycle/Splash.java
@@ -0,0 +1,25 @@
+package com.simraniks.gocycle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        getSupportActionBar().hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+            }
+        }, 3000);
+    }
+}
